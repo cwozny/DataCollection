fca9a3a273484bfd2185c910a32614d9d9ce0b28
Chris Wozny <1447143+cwozny@users.noreply.github.com>
2023-09-09 08:14:43 -0400

    Update ViewController.m

--- DataCollectionCommon/ViewController.m
+++ DataCollectionCommon/ViewController.m
@@ -3,7 +3,7 @@
 //  Data Collection
 //
 //  Created by Chris Wozny on 10/22/11.
-//  Copyright (c) 2011 Chris Wozny. All rights reserved.
+//  Copyright (c) 2013 Chris Wozny. All rights reserved.
 //
 
 #import "ViewController.h"
